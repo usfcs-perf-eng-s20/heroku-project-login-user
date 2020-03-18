a8dd6998d54d9851a72ce61637eb9b3f19689fce
Dharti Madeka <dpmadeka@dons.usfca.edu>
2020-03-17 21:01:34 -0700

    added class for edr and created json string to attached in requestBody

--- src/main/java/com/example/loginuser/controller/UserController.java
+++ src/main/java/com/example/loginuser/controller/UserController.java
@@ -4,10 +4,13 @@ package com.example.loginuser.controller;
 
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.example.loginuser.model.Users;
+import com.example.loginuser.model.EdrForm;
 import com.example.loginuser.service.UpdateService;
 import com.example.loginuser.service.UserRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,8 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,7 +30,7 @@ import java.util.Map;
 public class UserController {
     String loginCookieName = "isloggedIn";
     Cookie cookie = new Cookie(loginCookieName, "true");
-
+    EdrForm edr;
     @Autowired
     private UserRepository userRepository;
 
@@ -41,20 +46,32 @@ public class UserController {
 
     @GetMapping("/")
     public String hello() {
-        return "hello";
+    	//Instant start = Instant.now();
+    	String hello = "hello";
+        //Instant stop = Instant.now();
+        //edr = new EdrForm("get", "/user", (int)Duration.between(start, stop).toMillis(), "200", "login-team", true, Long.toString(System.currentTimeMillis()), "test");
+        //saveEdr(edr);
+        return hello;
     }
 
     //this is dummy api and u can see all users here--> remove it once we implement everything
+    
     @GetMapping("/user")
     List<Users> getUser()
     {
-        return (List<Users>) userRepository.findAll();
+    	Instant start = Instant.now();
+        List<Users> result = (List<Users>) userRepository.findAll();
+        Instant stop = Instant.now();
+        edr = new EdrForm("get", "/user", (int)Duration.between(start, stop).toMillis(), "200", "login-team", true, Long.toString(System.currentTimeMillis()), "test");
+        saveEdr(edr);
+        return result;
     }
 
     @GetMapping("/isLoggedIn") //this is for other services to check 
 //    whether user is logged in or not...we can do JWT in general and give them token if user is logged in
     public Map<String, Object> isLoggedIn(@RequestParam("userId") int userId, HttpServletRequest request, HttpServletResponse response)
         throws IOException {
+    	Instant startTime = Instant.now();
         List<Users> users  =  userRepository.findUserByID(userId);
         Map<String, Object> responseMap = new HashMap<>();
         if (users.size() >= 1) {
@@ -62,9 +79,18 @@ public class UserController {
         } else {
             //user doesn't exist
             response.sendError(500);
-
             System.out.println("Uer dones't exist");
         }
+        int responseCode = response.getStatus();
+        boolean successValue = false;
+        if(responseCode == 200)
+        {
+        	successValue = true;
+        }
+        Instant stopTime = Instant.now();
+        //System.out.println(users);
+        edr = new EdrForm(request.getMethod(), request.getPathInfo(), (int)Duration.between(startTime, stopTime).toMillis(), Integer.toString(responseCode), "login-service", successValue, Long.toString(System.currentTimeMillis()), users.get(userId).getUserName());  
+        saveEdr(edr);
         return responseMap;
     }
 
@@ -99,13 +125,9 @@ public class UserController {
         } catch (@SuppressWarnings("hiding") IOException e) {
             e.printStackTrace();
         }
-
+        
         return new ResponseEntity<Object>(responseMap, HttpStatus.OK);
     }
-
-
-
-
 
     @PostMapping(path = "/signup", consumes = "application/json", produces = "application/json")
     public ResponseEntity<?> signup(@RequestBody String jsonString)  {
@@ -148,10 +170,17 @@ public class UserController {
         return new ResponseEntity(responseMap, HttpStatus.OK);
     }
 
-    @GetMapping("/pageView")
-    public String pageView() {
-        return "";
-    }
     
     //implement save-edr api 
+    @PostMapping(path = "/saveEdr", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> saveEdr(EdrForm edr)
+    {
+    	Gson gson = new Gson();
+    	String jsonString = gson.toJson(edr); // this gives me request body
+    	System.out.println(jsonString);
+    	//now attached this requestBody with api of analytics team and send them. 
+//    	Do this for all API's
+		return null;
+    	
+    }
 }

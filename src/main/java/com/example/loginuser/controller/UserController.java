13ddd4ae09cd4510bc0dc37bbea261080713a942
zechnegli <zechengli1997@gmail.com>
2020-03-25 22:32:56 -0700

    Update UserController.java

--- src/main/java/com/example/loginuser/controller/UserController.java
+++ src/main/java/com/example/loginuser/controller/UserController.java
@@ -48,35 +48,19 @@ public class UserController {
     //read config files and get values for config and all
     Config config = readJsonFile("config.json");
    
-    //TODO:// this is dummy remove it while cleaning 
+
     @GetMapping("/user")
     List<Users> getUser()
     {
-    	System.out.println(config.isAnalytics());
-    	Instant start = Instant.now();
+        System.out.println(config.isAnalytics());
+        Instant start = Instant.now();
         List<Users> result = (List<Users>) userRepository.findAll();
         Instant stop = Instant.now();
         edr = new EdrForm("get", "/user", (int)Duration.between(start, stop).toMillis(), "200", "loginService/user", true, Long.toString(System.currentTimeMillis()), "test");
-        if (config.isAnalytics()) {
-            saveEdr(edr);
-            System.out.println("call anaylistic team");
-        } else {
-            System.out.println("not call anaylistic team");
-        }
+        saveEdr(edr);
         return result;
     }
 
-
-//    @GetMapping("config")
-//    void config(@RequestParam(defaultValue = "false", required=false) String analytics) {
-//        if (analytics != null && analytics.equals("true")) {
-//            willSaveEdr = true;
-//            System.out.println("call anaylistic team");
-//        } else {
-//            willSaveEdr = false;
-//            System.out.println("not call anaylistic team");
-//        }
-//    }
     
     @PutMapping("/config")
     public ResponseEntity<?> updateConfig(@RequestBody Config jsonString) {
@@ -91,12 +75,13 @@ public class UserController {
     	return new ResponseEntity<Object>(responseMap, HttpStatus.OK);
     }
 
-    //MOST IMPORTANT API
-    @GetMapping("/isLoggedIn") //this is for other services to check
+
+    @GetMapping("/isLoggedIn")
     public ResponseEntity<?> isLoggedIn( @RequestParam("userId") int userId, HttpServletRequest request, HttpServletResponse response)
         throws IOException {
-    	Instant startTime = Instant.now(); //for save-edr
+    	  Instant startTime = Instant.now(); //for save-edr
         HttpStatus responseStatus = HttpStatus.OK;
+        //check user based on user id
         Users user  =  userRepository.findUserByID(userId);
         Map<String, Object> responseMap = new HashMap<>();
         if (user != null) {
@@ -117,47 +102,45 @@ public class UserController {
         }
         Instant stopTime = Instant.now();
         edr = new EdrForm(request.getMethod(), request.getRequestURI(), (int)Duration.between(startTime, stopTime).toMillis(), Integer.toString(responseCode), "loginService/isLoggedIn", successValue, Long.toString(System.currentTimeMillis()), userName);
-        if (config.isAnalytics()) {
-            saveEdr(edr);
-        }
+        saveEdr(edr);
         return new ResponseEntity<Object>(responseMap, responseStatus);
     }
 
     
     @PostMapping(path = "/login", consumes ="application/json", produces = "application/json")
     public ResponseEntity<?> login(@RequestBody String jsonString, HttpServletResponse response, HttpServletRequest request) {
-    	Instant startTime = Instant.now(); //for save-edr
-    	ObjectMapper mapper = new ObjectMapper();
-    	List<Users> users = null;
+        Instant startTime = Instant.now(); //for save-edr
+        ObjectMapper mapper = new ObjectMapper();
+        List<Users> users = null;
         Map<String, Object> responseMap = new HashMap<>();
         int id = -1;
         String userName = "";
         HttpStatus responseStatus = HttpStatus.OK;
         //Convert JSON to POJO
         try {
+            //check if the user exists or not
             Users checkUser = mapper.readValue(jsonString, Users.class);
             users = userRepository.findByEmailAndPassword(checkUser.getEmail(), checkUser.getPassword());
             if (users.size() >= 1) {
+                //user is found and log in current user
             	userName = users.get(0).getUserName();
-                System.out.println("+++");
                 id = users.get(0).getUserId();
                 responseMap.put("userId", id);
                 int row = updateService.loggedIn(true, id);
 
             }else {
-
+                //user is not found
             	userName = userRepository.findUserNameByEmail(checkUser.getEmail());
             	//set responsecode
             	if(userName == null || userName == "")
-            	{	response.setStatus(400);
-            		responseStatus = HttpStatus.BAD_REQUEST;
-            	}
+                  {	response.setStatus(400);
+                    responseStatus = HttpStatus.BAD_REQUEST;
+                  }
             	else {
-					response.setStatus(401);
-					responseStatus = HttpStatus.UNAUTHORIZED;
-				}
+                  response.setStatus(401);
+                  responseStatus = HttpStatus.UNAUTHORIZED;
+            	}
             	responseMap.put("error", "incorrect email / password");
-                //set failed login in cookie
             }
 
         } catch (JsonProcessingException e) {
@@ -171,9 +154,7 @@ public class UserController {
         }
         Instant stopTime = Instant.now();
         edr = new EdrForm(request.getMethod(), request.getRequestURI(), (int)Duration.between(startTime, stopTime).toMillis(), Integer.toString(responseCode), "loginService/login", successValue, Long.toString(System.currentTimeMillis()), userName);
-        if (config.isAnalytics()) {
-            saveEdr(edr);
-        }
+        saveEdr(edr);
         return new ResponseEntity<Object>(responseMap, responseStatus);
     }
 
@@ -221,9 +202,7 @@ public class UserController {
             userName = users.get(0).getUserName();
         }
         edr = new EdrForm(request.getMethod(), request.getRequestURI(), (int)Duration.between(startTime, stopTime).toMillis(), Integer.toString(responseCode), "loginService/signup", successValue, Long.toString(System.currentTimeMillis()), userName);
-        if (config.isAnalytics()) {
-            saveEdr(edr);
-        }
+        saveEdr(edr);
         return new ResponseEntity(responseMap, responseStatus);
     }
 
@@ -262,74 +241,75 @@ public class UserController {
 
         if (responseMap.containsKey("error")) {
             edr = new EdrForm(request.getMethod(), request.getRequestURI(), (int) Duration.between(startTime, stopTime).toMillis(), Integer.toString(responseCode), "loginService/getUserInfo", successValue, Long.toString(System.currentTimeMillis()), "");
-            if (config.isAnalytics()) {
-                saveEdr(edr);
-            }
+            saveEdr(edr);
         } else {
             for (String userName : userNames) {
                 edr = new EdrForm(request.getMethod(), request.getRequestURI(), (int) Duration.between(startTime, stopTime).toMillis(), Integer.toString(responseCode), "loginService/getUserInfo", successValue, Long.toString(System.currentTimeMillis()), userName);
-                if (config.isAnalytics()) {
-                    saveEdr(edr);
-                }
+                saveEdr(edr);
             }
         }
         return new ResponseEntity(responseMap, responseStatus);
     }
 
-//    @PostMapping(path = "/saveEdr", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> saveEdr(EdrForm edr) {
-    	Gson gson = new Gson();
-    	String jsonString = gson.toJson(edr); // this gives me request body
-    	System.out.println("String = " + jsonString);
-        try {
-        	URL url = new URL ("http://analytics-boot.herokuapp.com/saveEdr");
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json; utf-8");
-            con.setRequestProperty("Accept", "application/json");
-            con.setDoOutput(true);
-            try(OutputStream os = con.getOutputStream()) {
-                byte[] input = jsonString.getBytes("utf-8");
-                os.write(input, 0, input.length);
-            } catch (IOException e) {
-                System.out.println(e);
-            }
-            try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), "utf-8"))) {
-                StringBuilder response = new StringBuilder();
-                String responseLine = null;
-                while ((responseLine = br.readLine()) != null) {
-                    response.append(responseLine.trim());
-                }
-                System.out.println("response" + response.toString());
-            } catch (IOException e) {
-                System.out.println(e);
-            }
-            System.out.println("response code from savedir" + con.getResponseCode());
-        } catch (IOException e) {
-            System.out.println("response form savedir" + 400);
-            System.out.println( e.getStackTrace());
 
+    public ResponseEntity<?> saveEdr(EdrForm edr) {
+        if (config.isAnalytics()) {
+            System.out.println("call anaylistic team");
+            Gson gson = new Gson();
+            String jsonString = gson.toJson(edr); // this gives me request body
+            System.out.println("String = " + jsonString);
+            try {
+                URL url = new URL ("http://analytics-boot.herokuapp.com/saveEdr");
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Content-Type", "application/json; utf-8");
+                con.setRequestProperty("Accept", "application/json");
+                con.setDoOutput(true);
+                try(OutputStream os = con.getOutputStream()) {
+                    byte[] input = jsonString.getBytes("utf-8");
+                    os.write(input, 0, input.length);
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+                try(BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                    StringBuilder response = new StringBuilder();
+                    String responseLine = null;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+                    System.out.println("response" + response.toString());
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+                System.out.println("response code from savedir" + con.getResponseCode());
+            } catch (IOException e) {
+                System.out.println("response form savedir" + 400);
+                System.out.println( e.getStackTrace());
+
+            }
+        } else {
+            System.out.println("not call anaylistic team");
         }
-        //TODO:// dont know what will come here, Are we suppose to pass some response or change it to void
+
         return null;
     }
     
-    Config readJsonFile(String filename)
-	{
-		Config config = new Config();
-		Gson gson = new Gson();
-		System.out.println(filename);
-		if(filename != null)
-		{
-			try {
-				JsonReader reader = new JsonReader(new FileReader(filename));
-				config = gson.fromJson(reader, Config.class);
-			} catch (FileNotFoundException e) 
-			{
-				System.exit(0);
-			}
-		}
-		return config;
-	}
+    public Config readJsonFile(String filename)
+      {
+        Config config = new Config();
+        Gson gson = new Gson();
+        System.out.println(filename);
+        if(filename != null)
+        {
+          try {
+            JsonReader reader = new JsonReader(new FileReader(filename));
+            config = gson.fromJson(reader, Config.class);
+          } catch (FileNotFoundException e)
+          {
+            System.exit(0);
+          }
+        }
+        return config;
+      }
 }
